1529ab390f602d57a3529489a1af768f65c7aec2
Oliver Singer <owsinger@outlook.com>
2022-12-09 14:31:32 +0000

    implemented

--- things/forms.py
+++ things/forms.py
@@ -10,24 +10,24 @@ class ThingForm(forms.ModelForm):
         model = Thing
         fields = ("name", "description", "quantity")
 
-            name = forms.CharField(label="name",
-                             widget=forms.TextInput(attrs={"placeholder": "e.g. John"}))
-            description = forms.CharField(label="Description",
-                             widget=forms.Textarea(attrs={"placeholder": "description here"}))
+        name = forms.CharField(label="name",
+                            widget=forms.TextInput(attrs={"placeholder": "e.g. John"}))
+        description = forms.CharField(label="Description",
+                            widget=forms.Textarea(attrs={"placeholder": "description here"}))
 
-            quantity = forms.CharField(label="quantity",
-                             widget=forms.NumberInput(attrs={"placeholder": "e.g. 88"}))
+        quantity = forms.CharField(label="quantity",
+                            widget=forms.NumberInput(attrs={"placeholder": "e.g. 88"}))
 
 
-            if quantity < 0 or quantity > 50:
-                self.add_error("quantity", "Cannot be blank, less than 0, or more than 50")
-            if len(description) > 120:
-                self.add_error("description", "Cannot be longer than 120 characters")
+        if quantity < 0 or quantity > 50:
+            self.add_error("quantity", "Cannot be blank, less than 0, or more than 50")
+        if len(description) > 120:
+            self.add_error("description", "Cannot be longer than 120 characters")
 
-            thing = Thing.objects.create_user(
-            self.cleaned_data.get("name"),
-            description=self.cleaned_data.get("description"),
-            quantity=self.cleaned_data.get("quantity"),
-        )
+        thing = Thing.objects.create_user(
+        self.cleaned_data.get("name"),
+        description=self.cleaned_data.get("description"),
+        quantity=self.cleaned_data.get("quantity"),
+    )
 
-        return thing
+    return thing
